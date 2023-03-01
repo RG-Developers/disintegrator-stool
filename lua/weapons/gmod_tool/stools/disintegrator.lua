42d0d6efc81e49cd2dd4cf262485e352a9dcd845
CommandLine <71168720+Def-Try@users.noreply.github.com>
2023-03-01 17:21:40 +0300

    Add files via upload

--- lua/weapons/gmod_tool/stools/disintegrator.lua
+++ lua/weapons/gmod_tool/stools/disintegrator.lua
@@ -1,230 +1,142 @@
 TOOL.Category = "Construction"
-
 TOOL.Name = "#tool.disintegrator.name"
 
 TOOL.Information = {
-
-	{ name = "left" },	{ name = "right" },
-
+	{ name = "left" },	
+	{ name = "right" },
 	{ name = "reload" }
-
 }
 
-local function DoRemoveEntity( ent, requester )
-
-    if ( !IsValid( ent ) or
-
-    ( ent:IsPlayer() && !requester:IsAdmin() ) ) then return false end
+local function DoRemoveEntity( entity, requester )
+	local ent = entity
+    if ( !IsValid( ent ) or ( ent:IsPlayer() && !requester:IsAdmin() ) ) then return false end
 
 	-- Nothing for the client to do here
-
 	if ( CLIENT ) then return true end
 
 	-- Remove all constraints (this stops ropes from hanging around)
-
 	constraint.RemoveAll( ent )
 
-	local t = 2 -- Effect time
-
-	-- Remove it properly in ~1 second
-
-	timer.Simple( t-0.01, function() if ( IsValid( ent ) ) then
-
-		ent:SetNoDraw( true )
-
-		local ed = EffectData()
-
-			ed:SetOrigin( ent:GetPos() )
-
-			ed:SetEntity( ent )
-
-		util.Effect( "entity_remove", ed, true, true )
-
-		timer.Simple( 0.5, function() if ( IsValid( ent ) ) then
-
-			ent:Remove()
-
-		end end )
-
-	end end )
+	local t = 1 -- Effect time
 
 	if ent:IsNPC() then
-
-		for i=1,50 do
-
-			timer.Simple( t*i/50, function() if ( IsValid( ent ) ) then 
-
-				ent:SetHealth(ent:Health()+5)
-
-				ent:TakeDamage(5)
-
-				local effectdata = EffectData()
-
-					effectdata:SetOrigin(ent:GetPos())
-
-				    effectdata:SetEntity(ent)
-
-				util.Effect("npc_disentigration", effectdata)
-
-			end end)
-
-		end
-
-		timer.Simple( t-0.01, function() if ( IsValid( ent ) ) then
-
-			local oldrags = ents.FindByClass("prop_ragdoll")
-
-			ent:TakeDamage(ent:Health())
-
-			local newrags = ents.FindByClass("prop_ragdoll")
-
-			for i in pairs(newrags) do
-
-				for j in pairs(oldrags) do
-
-					if i == j then table.remove(newrags, j) break end
-
-				end
-
+		local oldrags = ents.FindByClass("prop_ragdoll")
+		while ent:Health() > 0 do ent:TakeDamage(1) end
+		local newrags = ents.FindByClass("prop_ragdoll")
+		for i in pairs(newrags) do
+			for j in pairs(oldrags) do
+				if i == j then table.remove(newrags, j) break end
 			end
-
-			ent = newrags[1]
-
-		end end)
-
-	elseif ent:IsPlayer() then
-
-	     i=1,50 do
-
-			timer.Simple( t*i/50, function() if ( IsValid( ent ) ) then 
-
-				ent:SetHealth(ent:Health()+5)
-
-				ent:TakeDamage(5)
-
-				local effectdata = EffectData()
-
-					effectdata:SetOrigin(ent:GetPos())
-
-				    effectdata:SetEntity(ent)
-
-				util.Effect("npc_disentigration", effectdata)
-
-				util.Effect("disentigration", effectdata)
-
-			end end)
-
 		end
-
-		timer.Simple( t-0.01, function() if ( IsValid( ent ) ) then
-
-		    ent:Kick("You was disintegrated...")
-
-			ent = ent:GetRagdollEntity()
-
-		end end)
-
-	else
-
+		ent = newrags[1]
 		for i=1,50 do
-
 			timer.Simple( t*i/50, function() if ( IsValid( ent ) ) then 
-
 				local effectdata = EffectData()
-
 					effectdata:SetOrigin(ent:GetPos())
-
 				    effectdata:SetEntity(ent)
-
-				util.Effect("disentigration", effectdata)
-
+				util.Effect("npc_disintegration", effectdata)
 			end end)
-
 		end
-
+	elseif ent:IsPlayer() then
+		player.GetAll()[entity:EntIndex()]:SetNWBool("disintegration", true) 
+		local oldrags = ents.FindByClass("prop_ragdoll")
+		while ent:Health() > 0 do ent:TakeDamage(1) end
+		local newrags = ents.FindByClass("prop_ragdoll")
+		for i in pairs(newrags) do
+			for j in pairs(oldrags) do
+				if i == j then table.remove(newrags, j) break end
+			end
+		end
+		ent = newrags[1]
+		for i=1,50 do
+			timer.Simple( t*i/50, function() if ( IsValid( ent ) ) then 
+				local effectdata = EffectData()
+					effectdata:SetOrigin(ent:GetPos())
+				    effectdata:SetEntity(ent)
+				util.Effect("ply_disintegration", effectdata)
+			end end)
+		end
+	else
+		for i=1,50 do
+			timer.Simple( t*i/50, function() if ( IsValid( ent ) ) then 
+				local effectdata = EffectData()
+					effectdata:SetOrigin(ent:GetPos())
+				    effectdata:SetEntity(ent)
+				util.Effect("prop_disintegration", effectdata)
+			end end)
+		end
 	end
 
 	-- Make it non solid
-
-	ent:SetNotSolid( true )
-
+	ent:SetNotSolid( false )
 	ent:SetMoveType( MOVETYPE_NONE )
 
 	-- Send Effect
-
 	local ed = EffectData()
-
 		ed:SetOrigin( ent:GetPos() )
-
 		ed:SetEntity( ent )
+	util.Effect( "ent_despawn", ed, true, true )
 
-	util.Effect( "prop_remove", ed, true, true )
+	-- Remove it properly later
+	timer.Simple( t-0.01, function() 
+		if ( IsValid( ent ) ) then
+			ent:Remove()
+		end 
+		if ( IsValid( entity ) ) then
+			if entity:IsPlayer() then 
+				player.GetAll()[entity:EntIndex()]:SetNWBool("disintegration", false) 
+				player.GetAll()[entity:EntIndex()]:Kick("You was disintegrated...") 
+			else 
+				entity:Remove() 
+			end
+		end
+	end )
 
 	return true
 
 end
 
 function TOOL:LeftClick( trace )
-
 	if ( DoRemoveEntity( trace.Entity, self:GetOwner() ) ) then
-
 		if ( !CLIENT ) then
-
 			self:GetOwner():SendLua( "achievements.Remover()" )
-
 		end
-
 		return true
-
 	end
-
 	return false
 
 end
 
 function TOOL:RightClick( trace )
-
 	local Entity = trace.Entity
-
 	if ( !IsValid( Entity ) || Entity:IsPlayer() ) then return false end
-
 	if ( CLIENT ) then return true end
-
 	local ConstrainedEntities = constraint.GetAllConstrainedEntities( trace.Entity )
-
 	local Count = 0
-
 	-- Loop through all the entities in the system
-
 	for _, Entity in pairs( ConstrainedEntities ) do
-
 		if ( DoRemoveEntity( Entity, self:GetOwner() ) ) then
-
 			Count = Count + 1
-
 		end
-
 	end
-
 	self:GetOwner():SendLua( string.format( "for i=1,%i do achievements.Remover() end", Count ) )
-
 	return true
 
 end
 
 function TOOL:Reload( trace )
-
 	if ( !IsValid( trace.Entity ) || trace.Entity:IsPlayer() ) then return false end
-
 	if ( CLIENT ) then return true end
-
 	return constraint.RemoveAll( trace.Entity )
 
 end
 
 function TOOL.BuildCPanel( CPanel )
-
 	CPanel:AddControl( "Header", { Description = "#tool.disintegrator.desc" } )
-
 end
 
+hook.Add("PlayerDeathThink", "disintegration", function(ply)
+	if ply:GetNWBool("disintegration") != nil or ply:GetNWBool("disintegration") == true then 
+		return false
+	end
+end)
